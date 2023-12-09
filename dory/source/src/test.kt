514549e93ab00e40244b0e79a2f9b31612073973
dain7 <dainnnnnn96@gmail.com>
2023-12-09 21:18:19 +0900

    power tool

--- dory/source/src/test.kt
+++ dory/source/src/test.kt
@@ -1,43 +1,32 @@
+open class UserApiException(
+        open val errorCode: ResultCode,
+        override val message: String?,
+        open val extraMessage: String? = null
+) : RuntimeException(message)
 
-// MemberReferences/MemberReferencesEx1.kt
-package memberReferencesExercise1
-import memberReferencesExercise1.Habitat.*
-
-data class Pet(
-        var name: String,
-        val habitat: Habitat
-) {
-    override fun toString() = name
+data class SystemException(
+        override val errorCode: ResultCode,
+        override val message: String,
+        override val extraMessage: String? = null
+) : UserApiException(errorCode, message, extraMessage) {
+    constructor(extraMessage: String?) : this(ResultCode.INTERNAL_SERVER_ERROR, ResultCode.INTERNAL_SERVER_ERROR.message, extraMessage)
 }
 
-enum class Habitat {
-    LAND, WATER, AMPHIBIOUS;
-    fun livesIn(pet: Pet) = pet.habitat == this
+@Suppress("unused")
+enum class ResultCode(val message: String) {
+    SUCCESS("ok"),
+    INTERNAL_SERVER_ERROR("system error occurred!")
 }
-
-fun List<Pet>.liveOnLand(): List<Pet> =
-        this.filter(LAND::livesIn)
-
-fun List<Pet>.liveInWater(): List<Pet> =
-        this.filter { it.habitat == WATER }
-
-fun List<Pet>.areAmphibious(): List<Pet> =
-        this.filter(AMPHIBIOUS::livesIn)
-
-fun List<Pet>.partitionAmphibious(): Pair<List<Pet>, List<Pet>> =
-        this.partition { it.habitat == AMPHIBIOUS }
 
 fun main() {
-    val pets = listOf(
-            Pet("Dog", LAND),
-            Pet("Cat", LAND),
-            Pet("Goldfish", WATER),
-            Pet("Turtle", AMPHIBIOUS),
-            Pet("Frog", AMPHIBIOUS)
-    )
-    println(pets.liveOnLand().toString() == "[Dog, Cat]") // 육지동물
-    println(pets.liveInWater().toString() == "[Goldfish]")  // 수중동물
-    pets.areAmphibious().toString() == "[Turtle, Frog]"    // 양서류
-    println(pets.partitionAmphibious().toString() ==               // 양서류와 양서류 아닌 것
-            "([Turtle, Frog], [Dog, Cat, Goldfish])")
+    try {
+        val s = "abc"
+        for (i in 0..s.lastIndex+1) {
+            print(s[i] + 1)
+        }
+    } catch (e: Exception) {
+        val customException = SystemException("String index out of range: 3")
+        assert("bcdSystemException(errorCode=INTERNAL_SERVER_ERROR, message=system error occurred!, extraMessage=String index out of range: 3)" == customException.toString())
+        e.printStackTrace()
+    }
 }
